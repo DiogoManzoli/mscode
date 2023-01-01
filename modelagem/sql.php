7cd53bc1c855d64b75cc1acfc4620544b06dbd92
Diogo Manzoli <85167436+DiogoManzoli@users.noreply.github.com>
2023-01-01 17:57:03 -0300

    Removendo código duplicado.

--- modelagem/sql.php
+++ modelagem/sql.php
@@ -7,12 +7,3 @@ CREATE TABLE `msavalie`.`avaliacao`(
     stars VARCHAR(1) NOT NULL,
     text VARCHAR(255) NULL
 );
-CREATE SCHEMA IF NOT EXISTS `msavalie` DEFAULT CHARACTER SET utf8 ;
-
-DROP TABLE IF EXISTS `msavalie`.`avaliacao` ;
-
-CREATE TABLE `msavalie`.`avaliacao`(
-	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    stars VARCHAR(1) NOT NULL,
-    text VARCHAR(255) NULL
-);
